af124a20543495cbd246a5285051377a753cdf47
Oscar Morrison <github@oscarmorrison.com>
2025-03-10 21:32:09 +1100

    upate syntax

--- nscripts.js
+++ nscripts.js
@@ -1,17 +1,27 @@
 #! /usr/bin/env node
-const chalk = require('chalk');
-const dir = process.cwd()+'/package.json';
+import chalk from 'chalk';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const dir = join(process.cwd(), 'package.json');
 let packageJSON;
 
 try {
-     packageJSON = require(dir);
+    const rawData = readFileSync(dir, 'utf8');
+    packageJSON = JSON.parse(rawData);
 } catch(e) {
     console.error('no package.json');
-    return false;
+    process.exit(1);
 }
 
-let { scripts } = packageJSON;
+const { scripts } = packageJSON;
+
+if (!scripts) {
+    console.error('no scripts found in package.json');
+    process.exit(1);
+}
 
 Object.keys(scripts).forEach(key => {
-    console.log(chalk.black.bgGreen.bold(' '+key+': ')+chalk.inverse(' '+scripts[key]+' \n'));
+    console.log(chalk.black.bgGreen.bold(` ${key}: `) + chalk.inverse(` ${scripts[key]} \n`));
 });
